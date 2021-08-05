c36e47ee48438e575f91c36c7f1ae52dca75b1fe
Vishnu Lokhande <lokhande@cs.wisc.edu>
2021-08-05 18:59:15 +0000

    add run scripts

---
+++ bin/run_celeba_erm.sh
@@ -0,0 +1,11 @@
+python train_and_eval_loop.py \
+       --dataset 'CelebA' \
+       --model_type 'resnet50' \
+       --method 'erm' \
+       --optimizer 'SGD' \
+       --learning_rate 1e-5 \
+       --noflag_saveckpt \
+       --batch_size 128 \
+       --num_epoch 50 \
+       --weight_decay 0.1 \
+       --gpu_ids '0'

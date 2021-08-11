04dc55a3ce6b8c5de786719694dddd2f56200ac9
Vishnu Lokhande <lokhande@cs.wisc.edu>
2021-08-11 04:00:31 +0000

    bin file

---
+++ bin/run_celeba_worstoffdro.sh
@@ -0,0 +1,15 @@
+python train_and_eval_loop.py \
+       --dataset 'CelebA' \
+       --model_type 'resnet50' \
+       --method 'worstoffdro' \
+       --optimizer 'SGD' \
+       --learning_rate 1e-5 \
+       --noflag_saveckpt \
+       --batch_size 128 \
+       --num_epoch 50 \
+       --weight_decay 0.1  \
+       --lab_split 0.3 \
+       --worstoffdro_lambda 0.01 \
+       --worstoffdro_latestart 15 \
+       --gpu_ids '6' 
+       

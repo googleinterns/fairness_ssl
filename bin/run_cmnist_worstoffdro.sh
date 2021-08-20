c6a6771209c9d995af17f008d5c2f54145367cec
Vishnu Lokhande <lokhande@cs.wisc.edu>
2021-08-20 05:28:22 +0000

    bin file

--- bin/run_cmnist_worstoffdro.sh
+++ bin/run_cmnist_worstoffdro.sh
@@ -5,13 +5,13 @@ python train_and_eval_loop.py \
        --method 'worstoffdro' \
        --optimizer 'Adam' \
        --learning_rate 0.0005 \
+       --batch_size 39640 \
        --noflag_saveckpt \
-       --batch_size 128 \
        --num_epoch 501 \
        --weight_decay 0.001  \
-       --lab_split 0.3 \
+       --lab_split 0.1 \
        --worstoffdro_lambda 1.0 \
        --worstoffdro_latestart 0 \
-       --worstoffdro_marginals=.5,.4,.1 \
-       --gpu_ids '3'
+       --worstoffdro_marginals=.53,.40,.07 \
+       --gpu_ids '0' 
        

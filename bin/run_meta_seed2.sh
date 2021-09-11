52c35aad0b60881d66e9462a8a155f546df2759b
Vishnu Lokhande <lokhande@cs.wisc.edu>
2021-09-11 03:03:10 +0000

    run meta seed

---
+++ bin/run_meta_seed2.sh
@@ -0,0 +1,13 @@
+bash bin/run_all_cmnist_groupdro_partial30.sh;
+bash bin/run_all_cmnist_groupdro_partial60.sh;
+bash bin/run_all_cmnist_groupdro_partial90.sh;
+bash bin/run_all_cmnist_worstoffdro30.sh;
+bash bin/run_all_cmnist_worstoffdro60.sh;
+bash bin/run_all_cmnist_worstoffdro90.sh;
+bash bin/run_all_adult2_groupdro_partial30.sh;
+bash bin/run_all_adult2_groupdro_partial60.sh;
+bash bin/run_all_adult2_groupdro_partial90.sh;
+bash bin/run_all_adult2_worstoffdro30.sh;
+bash bin/run_all_adult2_worstoffdro60.sh;
+bash bin/run_all_adult2_worstoffdro90.sh;
+

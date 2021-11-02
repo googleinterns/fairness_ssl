f7dc68063fb34062280b5f7410cedaa10eb6d749
Vishnu Lokhande <lokhande@cs.wisc.edu>
2021-11-02 16:39:06 +0000

    rm extra bin files

--- bin/run_adult_erm.sh
+++
@@ -1,9 +0,0 @@
-python train_and_eval_loop.py \
-       --dataset 'Adult' \
-       --method 'erm' \
-       --noflag_usegpu \
-       --noflag_saveckpt \
-       --num_epoch 201 \
-       --learning_rate 0.001       
-
-
